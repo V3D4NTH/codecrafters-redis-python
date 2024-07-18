822a2362264d566c66cd60c2cd16246464aad39b
V3D4NTH <vedanth.sameer@gmail.com>
2024-07-18 13:08:17 +0530

    Respond to PING

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
-
+    conn, addr = server_socket.accept()
+    conn.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
