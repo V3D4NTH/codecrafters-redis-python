50a725aa24bebf6a93f1f905a1564c5cc8499351
V3D4NTH <vedanth.sameer@gmail.com>
2024-07-18 13:59:48 +0530

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 import socket
+from threading import Thread
 
-
-def main():
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, addr = server_socket.accept()
+def on_new_client(connection: socket.socket, addr: tuple[str, int]) -> None:
     with connection:
         print(f"Connected by {addr}")
         while True:
@@ -15,5 +11,16 @@ def main():
             connection.sendall(b"+PONG\r\n")
         connection.close()
 
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.listen(5)
+    while True:
+        connection, addr = server_socket.accept()
+        thread = Thread(target=on_new_client, args=(connection, addr))
+        thread.start()
+        
 if __name__ == "__main__":
     main()
